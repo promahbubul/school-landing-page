39b5cef40557d00774b62767a4531ff0073f5e3d
promahbubul <mahbublalam500@gmail.com>
2024-10-27 00:17:04 +0600

    update of review left area

---
+++ src/components/Home/Review/Left/index.jsx
@@ -0,0 +1,32 @@
+/* eslint-disable react/prop-types */
+import {
+  MdOutlineArrowBackIos,
+  MdOutlineArrowForwardIos,
+  underline,
+} from "../../../../assets/icons";
+
+const Left = () => {
+  return (
+    <div className="w-full md:w-4/12  flex items-end flex-row md:flex-col gap-6 md:gap-0">
+      <div className="text-2xl md:text-2xl md:text-[48px]  relative text-black font-bold font-open-sans">
+        <h1 className="md:leading-normal">
+          What our <span className="text-green">Students</span> say about us
+        </h1>
+        <img
+          src={underline}
+          alt=""
+          className="absolute top-16 w-32 left-0 md:left-auto md:w-auto md:top-[135px]"
+        />
+      </div>
+      <div className="mt-14 flex flex-row gap-6 md:gap-12">
+        <button className="bg-white  text-green hover:text-white border hover:bg-green shadow-md shadow-slate-400 border-green duration-500 p-5 rounded-full">
+          <MdOutlineArrowBackIos />
+        </button>
+        <button className="bg-white  text-green hover:text-white border hover:bg-green shadow-md shadow-slate-400 border-green duration-500 p-5 rounded-full">
+          <MdOutlineArrowForwardIos />
+        </button>
+      </div>
+    </div>
+  );
+};
+export default Left;
--- src/components/Home/Review/index.jsx
+++ src/components/Home/Review/index.jsx
@@ -1,18 +1,16 @@
 import SectionTitle from "../../shared/ui/SectionTitle";
 import Slider from "react-slick";
-
 import RightArea from "./RightArea";
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
-import LeftArea from "./leftArea";
-
+import Left from "./Left";
 
 const Review = () => {
   return (
     <section className="mt-24">
       <SectionTitle className={"text-center"} title={"Reviews"} />
       <div className="flex flex-col md:flex-row gap-9">
-        <LeftArea />
+        <Left />
         <RightArea Slider={Slider} />
       </div>
     </section>
